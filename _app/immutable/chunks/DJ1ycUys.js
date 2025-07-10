06357e3ab1c9377a5fa448724aa4c97e8ec7e567
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-07-10 20:33:23 +0300

    deploy

---
+++ _app/immutable/chunks/DJ1ycUys.js
@@ -0,0 +1,1 @@
+import"./CWj6FrbW.js";import{a1 as n,i as o,aV as e,j as p}from"./CxWMsIcg.js";function c(r,t){var a=n(),i=o(a);e(i,()=>t.children),p(r,a)}export{c as L};
---
+++ _app/immutable/nodes/0.sdpxXsxC.js
@@ -0,0 +1,1 @@
+import{L as m}from"../chunks/DJ1ycUys.js";export{m as component};
