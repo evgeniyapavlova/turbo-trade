200683baebe4fb4904089b7ac72fe25c70070803
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-07-10 20:48:53 +0300

    deploy

---
+++ _app/immutable/chunks/U5Y8TLSB.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1cit6m2)==null?void 0:s.base)??"/turbo-trade";var t;const e=((t=globalThis.__sveltekit_1cit6m2)==null?void 0:t.assets)??a;export{e as a,a as b};
