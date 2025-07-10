88c9173cc26a18cc73736f425d9ab0d406bbcdbc
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-07-10 20:27:37 +0300

    fix config

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,12 @@ const entries = locales.map((locale) => `/${locale}/index`);
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/turbo-trade' : ''
+		}
+	},
 	prerender: {
 		entries: [...entries, '/']
 	},
