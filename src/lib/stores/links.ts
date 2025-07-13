8720facfb64af95280bfbfa98cf3bd04716dfd39
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-07-13 05:46:13 +0300

    fix reg links

--- src/lib/stores/links.ts
+++ src/lib/stores/links.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store';
 
-export const login = writable('https://trade.turbo.trade.com/en/login');
+export const login = writable('https://trade.turbo.trade/en/login');
 
-export const reg = writable('https://trade.turbo.trade.com/en/register');
+export const reg = writable('https://trade.turbo.trade/en/register');
 
-export const searchString = writable('https://trade.turbo.trade.com/en/register');
+export const searchString = writable('');
